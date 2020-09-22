031ba6c30848cbcf42e9d3020212a511c9c279b7
kevinyieh <kevyieh@gmail.com>
2020-09-22 18:36:55 -0400

    Add splash background

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -3,8 +3,23 @@ import LoginFormContainer from "../login/login_form_container";
 
 export default function Splash() {
     return (
-        <div className="splash-container">
-            <LoginFormContainer />
+        <div className="splash">
+            <div className="splash-container">
+                <div className="splash-main">
+                    <div className="splash-greeting">
+                        <img src={window.logoUrl} />
+                        <h1>
+                            Connect with friends and the world around you on Friendlybook.
+                        </h1>
+                    </div>
+                    <LoginFormContainer />
+                </div>
+                
+            </div>
+            <div className="footer fine-print">
+                <div> English (US) </div>
+                <div className="separator" />
+            </div>
         </div>
     )    
 }
