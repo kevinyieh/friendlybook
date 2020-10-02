4fbc56f014635596db3e2b0e10d52da8f9d83a65
kevinyieh <kevyieh@gmail.com>
2020-10-02 11:03:29 -0400

    Fix updating for posts

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -35,7 +35,13 @@ class Api::PostsController < ApplicationController
         @post = Post.find_by(id: posts_params[:id])
         if @post
             if current_user.id === @post.user_id
-                @post.update(posts_params)
+                new_posts_params = posts_params
+                if new_posts_params[:photo].is_a?(String)
+                    new_posts_params.delete(:photo)
+                elsif new_posts_params[:photo].nil?
+                    @post.photo.purge if @post.photo.attached?
+                end
+                @post.update(new_posts_params)
                 @post = Post.retrieve_post(@post.id)
                 render :show
             else
