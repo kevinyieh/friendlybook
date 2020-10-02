a55e1d7f328e0e55399475eb1ad0cd3051dea92c
kevinyieh <kevyieh@gmail.com>
2020-10-02 00:24:45 -0400

    Remove functions from window for heroku push

--- frontend/friendly_book.jsx
+++ frontend/friendly_book.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Root from "./components/root";
 import configureStore from "./store/store";
-import * as SearchUtil from "./util/search_util";
-import { fetchAllFriends } from "./actions/friend_actions";
-import { fetchNewsfeed } from "./actions/post_actions";
+// import * as SearchUtil from "./util/search_util";
+// import { fetchAllFriends } from "./actions/friend_actions";
+// import { fetchNewsfeed } from "./actions/post_actions";
 
 document.addEventListener("DOMContentLoaded",() => {
     const root = document.getElementById("root");
@@ -37,11 +37,11 @@ document.addEventListener("DOMContentLoaded",() => {
     }else {
         store = configureStore();
     }
-    window.fetchAllFriends = fetchAllFriends;
-    window.fetchNewsfeed = fetchNewsfeed;
-    window.getState = store.getState;
-    window.fetchSearch = SearchUtil.fetchSearch;
-    window.dispatch = store.dispatch;
+    // window.fetchAllFriends = fetchAllFriends;
+    // window.fetchNewsfeed = fetchNewsfeed;
+    // window.getState = store.getState;
+    // window.fetchSearch = SearchUtil.fetchSearch;
+    // window.dispatch = store.dispatch;
     delete window.currentUser;
     ReactDOM.render(<Root store={store}/>, root );
 })
